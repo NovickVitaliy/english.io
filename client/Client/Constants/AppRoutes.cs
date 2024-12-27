0cd0173a6873603393533a58ef2d6c50c51f7d5b
Vitalii Novik <novickvitaliy@gmail.com>
2024-12-27 17:35:06 +0200

    feat: add landing page

---
+++ client/Client/Constants/AppRoutes.cs
@@ -0,0 +1,9 @@
+namespace Client.Constants;
+
+public static class AppRoutes
+{
+    public static class Landing
+    {
+        public const string Home = "/";
+    }
+}
--- client/Client/Program.cs
+++ client/Client/Program.cs
@@ -1,8 +1,19 @@
+using System.Net;
 using Client.Components;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddRazorComponents();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.Listen(IPAddress.Loopback, 4998);
+    options.Listen(IPAddress.Parse("192.168.1.225"), 4998);
+});
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
@@ -17,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
