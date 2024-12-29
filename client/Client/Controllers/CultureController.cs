fd43377f255cb51015fec57c6c7db910010f3391
Vitalii Novik <novickvitaliy@gmail.com>
2024-12-29 22:49:55 +0200

    feat: configure localization

---
+++ client/Client/Controllers/CultureController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+
+[Route("[controller]/[action]")]
+public class CultureController : Controller
+{
+    public IActionResult SetCulture(string? culture, string redirectUri)
+    {
+        if (culture is not null)
+        {
+            HttpContext.Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)));
+        }
+
+        return LocalRedirect(redirectUri);
+    }
+}
