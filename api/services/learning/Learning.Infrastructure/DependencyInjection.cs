162dda7b50143626ab3f7a3ae7a2994f6fe1e1d7
Vitalii Novik <novickvitaliy@gmail.com>
2025-01-09 16:47:00 +0200

    feat: setup db context for mongodb

---
+++ api/services/learning/Learning.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+using Learning.Infrastructure.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Learning.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection ConfigureInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions<MongoOptions>()
+            .BindConfiguration(MongoOptions.ConfigurationKey)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        services.AddSingleton<IMongoClient>(sp =>
+        {
+            var mongoOptions = sp.GetRequiredService<IOptions<MongoOptions>>().Value;
+
+            return new MongoClient(mongoOptions.ConnectionString);
+        });
+        
+        return services;
+    }
+}
