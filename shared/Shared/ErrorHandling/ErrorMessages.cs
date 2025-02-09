559dd832c651b06467a138b27928dcbd9e61c820
Vitalii Novik <novickvitaliy@gmail.com>
2025-02-09 16:01:22 +0200

    refactor code to use problem details

--- shared/Shared/ErrorHandling/ErrorMessages.cs
+++ shared/Shared/ErrorHandling/ErrorMessages.cs
@@ -1,19 +1,12 @@
 namespace Shared.ErrorHandling;
-//TODO: change class to use localization         
+
 public static class ErrorMessages
 {
-    public static string NoError()
-    {
-        return string.Empty;
-    }
-
-    public static string NotFound<TEntityType>(object key)
-    {
-        return $"Entity of type '{typeof(TEntityType).Name}' with key: '{key}' was not found.";
-    }
-
-    public static string BadRequest(string reason)
-    {
-        return $"Bad request. Reason: '{reason}'";
-    }
+    public const string NoError = "No error";
+    public static string BadRequest(string reason) => $"Bad Request: {reason}";
+    public static string Unauthorized(string reason) => $"Unauthorized: {reason}";
+    public static string Forbidden(string reason) => $"Forbidden: {reason}";
+    public static string NotFound<T>(object key) => $"{typeof(T).Name} with key '{key}' was not found.";
+    public static string Conflict(string reason) => $"Conflict: {reason}";
+    public static string InternalServerError(string reason) => $"Internal Server Error: {reason}";
 }
