668e9071fbc9416c805c7753e219375effc9deda
Vitalii Novik <novickvitaliy@gmail.com>
2025-02-10 15:44:47 +0200

    create service for client for forgot password functionality

--- client/modules/Authentication/Features/ForgotPassword/Components/ForgotPasswordForm.razor.cs
+++ client/modules/Authentication/Features/ForgotPassword/Components/ForgotPasswordForm.razor.cs
@@ -1,8 +1,12 @@
 using Authentication.Features.ForgotPassword.Models;
+using Authentication.Shared.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Options;
 using MudBlazor;
+using Refit;
+using Shared.Extensions;
 using Shared.Options;
+using ProblemDetails = Microsoft.AspNetCore.Mvc.ProblemDetails;
 
 namespace Authentication.Features.ForgotPassword.Components;
 
@@ -12,6 +16,8 @@ public partial class ForgotPasswordForm : ComponentBase
     private MudForm _form = null!;
 
     [Inject] private IOptions<ClientOptions> ClientOptions { get; init; } = null!;
+    [Inject] private IAuthenticationService AuthenticationService { get; init; } = null!;
+    [Inject] private ISnackbar Snackbar { get; init; } = null!;
 
     protected override void OnParametersSet()
     {
@@ -21,9 +27,20 @@ public partial class ForgotPasswordForm : ComponentBase
         };
     }
 
-    private static Task Submit()
+    private async Task Submit()
     {
-        return Task.CompletedTask;
+        await _form.Validate();
+        if (!_form.IsValid) return;
+        try
+        {
+            await AuthenticationService.ForgotPasswordAsync(_forgotPasswordRequest);
+            Snackbar.Add(Localizer["Email_Sent_Successfully"], Severity.Success);
+        }
+        catch (ApiException e)
+        {
+            var problemDetails = e.ToProblemDetails();
+            Snackbar.Add(problemDetails.Detail ?? problemDetails.Title ?? "Error Occured", Severity.Error);
+        }
     }
 }
 
--- client/modules/Authentication/Shared/Services/IAuthenticationService.cs
+++ client/modules/Authentication/Shared/Services/IAuthenticationService.cs
@@ -1,3 +1,4 @@
+using Authentication.Features.ForgotPassword.Models;
 using Authentication.Features.Login.Models;
 using Authentication.Features.Register.Models;
 using Authentication.Shared.Models;
@@ -8,10 +9,13 @@ namespace Authentication.Shared.Services;
 public interface IAuthenticationService
 {
     const string ApiUrlKey = "Authentication";
-    
+
     [Post("/auth/register")]
     Task<AuthResponse> RegisterAsync(RegisterRequest request);
 
     [Post("/auth/login")]
     Task<AuthResponse> LoginAsync(LoginRequest request);
+
+    [Post("/auth/forgot-password")]
+    Task ForgotPasswordAsync(ForgotPasswordRequest request);
 }
