2debd145fc34c17292036644c6359c16bcc7e8fc
Vitalii Novik <novickvitaliy@gmail.com>
2025-02-13 15:39:54 +0200

    feat: add decks toolbar on decks page

---
+++ client/modules/Learning/Features/Decks/Components/DecksToolbar.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Learning.Features.Decks.Components;
+
+public partial class DecksToolbar : ComponentBase
+{
+    private string _searchQuery = "";
+}
+
