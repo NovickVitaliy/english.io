b03b7d45ca7454fa22a36dcbca990da03bafc055
Vitalii Novik <novickvitaliy@gmail.com>
2025-02-19 15:19:13 +0200

    feat: add toolbar for deck

--- client/modules/Learning/Features/Decks/Pages/DeckPage.razor.cs
+++ client/modules/Learning/Features/Decks/Pages/DeckPage.razor.cs
@@ -2,6 +2,7 @@ using Fluxor;
 using Learning.Features.Decks.Models;
 using Learning.LearningShared.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
 using Shared.Store.User;
 
 namespace Learning.Features.Decks.Pages;
@@ -9,5 +10,6 @@ namespace Learning.Features.Decks.Pages;
 public partial class DeckPage : ComponentBase
 {
     [Parameter] public Guid DeckId { get; init; }
+    [Inject] private IStringLocalizer<DeckPage> Localizer { get; init; } = null!;
 }
 
