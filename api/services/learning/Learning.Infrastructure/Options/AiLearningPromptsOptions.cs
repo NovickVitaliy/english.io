16c7760e090d20525df9a72145d05798abdfd4b8
Vitalii Novik <novickvitaliy@gmail.com>
2025-03-07 18:30:26 +0200

    feature: implement checking if word complies to the topic

--- api/services/learning/Learning.Infrastructure/Options/AiLearningPromptsOptions.cs
+++ api/services/learning/Learning.Infrastructure/Options/AiLearningPromptsOptions.cs
@@ -9,4 +9,7 @@ public class AiLearningPromptsOptions
 
     [Required]
     public string PromptForWordTranslatingWithExampleSentences { get; init; } = null!;
+
+    [Required]
+    public string PromptForCheckingIfWordCompliesToTheTopic { get; init; } = null!;
 }
